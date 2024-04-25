dbdc8c7df61e9abf14aed665f02573f26b03b4d2
maaaashi <doyaseizin.7@gmail.com>
2024-04-25 22:49:21 +0900

    println

--- src/todo_tool/import.clj
+++ src/todo_tool/import.clj
@@ -1,14 +1,20 @@
 (ns todo-tool.import
-  (:require [taoensso.timbre :as log]
-            [clj-http.client :as http]))
-
+  (:require [cheshire.core :as json]
+           [clj-http.client :as http]
+           [clojure.core.async :as a]
+           [clojure.java.io :as io]
+           [concurrently.core :as c]
+           [databox.core :as d]
+           [taoensso.timbre :as log]))
 (defn- post-todo []
   (http/post "http://localhost:3000/todos"))
 
 (defn execute []
-  (let [file-name "data.json"]
-    (log/info "Importing:" file-name)))
+  (let [file-name "data2.json"]
+    (with-open [r (io/reader (io/resource file-name))]
+      (->> (json/parse-stream r true)
+           (map #(println %))))))
 
 (comment
-  (post-todo)
+  (execute)
   )
