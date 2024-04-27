ca127841ea2515429aa91fe374751f87743159bc
maaaashi <doyaseizin.7@gmail.com>
2024-04-27 22:23:26 +0900

    wip

--- src/todo_tool/import.clj
+++ src/todo_tool/import.clj
@@ -7,14 +7,20 @@
            [databox.core :as d]
            [todo-tool.todo :as todo]
            [taoensso.timbre :as log]))
-(defn- post-todo []
-  (http/post "http://localhost:3000/todos"))
+(defn- post-todo [todo]
+  (http/post "http://localhost:3000/todos"
+             {:form-params todo
+              :content-type :json
+              :as :json}))
 
 (defn execute []
   (let [file-name "data2.json"]
     (with-open [r (io/reader (io/resource file-name))]
       (->> (json/parse-stream r true)
-           (map todo/json->todo)))))
+           (map todo/json->todo)
+           (post-todo)
+           (:body)
+           (:status)))))
 
 (comment
   (execute)
