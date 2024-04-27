33e8b42d975e736c1920db2ec4395c27f597f03e
maaaashi <doyaseizin.7@gmail.com>
2024-04-28 00:38:10 +0900

    wip

--- src/todo_tool/import.clj
+++ src/todo_tool/import.clj
@@ -16,11 +16,26 @@
 (defn execute []
   (let [file-name "data2.json"]
     (with-open [r (io/reader (io/resource file-name))]
-      (->> (json/parse-stream r true)
-           (map todo/json->todo)
-           (post-todo)
-           (:body)
-           (:status)))))
+      (let [todos (->> (json/parse-stream r true)
+                  (map todo/json->todo)) 
+            shared-process (c/concurrent-process-blocking 1
+                                                          (a/chan 1)
+                                                          (d/map (fn [todo] (post-todo todo)))
+                                                          (a/chan 1)
+                                                          {:ordered? false})
+            input (a/chan 1)
+            p (a/go-loop [todos todos]
+                (if-let [todo (first todos)]
+                  (do (a/>! input todo)
+                      (recur (rest todo)))
+                  (a/close! input)))
+            {:keys [channel]} (c/concurrently shared-process
+                                              input
+                                              {:option-to-function true})
+            results (c/get-results!! channel {:catch #(log/error %)
+                                              :timeout-ms 8000})]
+        (a/<!! p)
+        results))))
 
 (comment
   (execute)
