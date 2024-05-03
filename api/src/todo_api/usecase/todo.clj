40d5dcf43d66c82949e9a94cc2127a52fee1e017
maaaashi <doyaseizin.7@gmail.com>
2024-05-03 23:03:59 +0900

    wip

--- api/src/todo_api/usecase/todo.clj
+++ api/src/todo_api/usecase/todo.clj
@@ -2,5 +2,7 @@
   (:require [todo-api.boundary.todo :as b]))
 
 (defn register [{:keys [todo-repository] :as _} todo]
+  (println "★★★★★★★★★★★★★★★")
   (when (satisfies? b/TodoRepository todo-repository)
+    (println "todo-repository is TodoRepository")
     (b/create-todo todo-repository todo)))
