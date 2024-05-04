2740a5f057a7f609e9b242800404b0203d151b25
maaaashi <doyaseizin.7@gmail.com>
2024-05-04 14:23:02 +0900

    usecase test

--- api/src/todo_api/usecase/todo.clj
+++ api/src/todo_api/usecase/todo.clj
@@ -1,8 +1,20 @@
 (ns todo-api.usecase.todo 
-  (:require [todo-api.boundary.todo :as b]))
+  (:require [todo-api.boundary.todo :as b]
+            [clojure.spec.alpha :as s]))
+
+(s/def ::id string?)
+(s/def ::title string?)
+(s/def ::content string?)
+(s/def ::todo (s/keys :req-un [::id ::title ::content]))
+
+(defn- register-todo [todo-repository todo]
+  (println "todo-repository is TodoRepository")
+  (b/create-todo todo-repository todo)
+  {:status "ok"})
 
 (defn register [{:keys [todo-repository] :as _} todo]
-  (println "★★★★★★★★★★★★★★★")
   (when (satisfies? b/TodoRepository todo-repository)
-    (println "todo-repository is TodoRepository")
-    (b/create-todo todo-repository todo)))
+    (println todo)
+    (if (s/valid? ::todo todo)
+      (register-todo todo-repository todo)
+      {:status "ng"})))
--- api/test/todo_api/usecase/todo_test.clj
+++ api/test/todo_api/usecase/todo_test.clj
@@ -5,7 +5,11 @@
             [shrubbery.core :as shr]))
 
 (t/deftest register-test
-  (t/testing "TODOを登録することができる" 
-    (let [todo {:id "id" :title "test"}
+  (t/testing "TODOを登録することができる"
+    (let [todo {:id "id" :title "test" :content "content"}
           todo-repository (shr/mock b/TodoRepository {:create-todo todo})]
-      (t/is (= (sut/register {:todo-repository todo-repository} todo) todo)))))
+      (t/is (= (sut/register {:todo-repository todo-repository} todo) {:status "ok"}))))
+  (t/testing "TODOの型が合わない場合、TODOを登録することができない"
+    (let [todo {:id "id"}
+          todo-repository (shr/mock b/TodoRepository {:create-todo todo})]
+      (t/is (= (sut/register {:todo-repository todo-repository} todo) {:status "ng"})))))
