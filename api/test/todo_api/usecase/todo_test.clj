7026574de13f8f4577733a3b51fb0b080e36b310
maaaashi <doyaseizin.7@gmail.com>
2024-05-04 12:57:22 +0900

    wip test

--- api/test/todo_api/usecase/todo_test.clj
+++ api/test/todo_api/usecase/todo_test.clj
@@ -2,13 +2,10 @@
   (:require [clojure.test :as t]
             [todo-api.boundary.todo :as b]
             [todo-api.usecase.todo :as sut]
-            [mockfn.macros :as mockfn]
-            [mockfn.matchers :as matchers]
             [shrubbery.core :as shr]))
 
 (t/deftest register-test
   (t/testing "TODOを登録することができる" 
     (let [todo {:id "id" :title "test"}
           todo-repository (shr/mock b/TodoRepository {:create-todo todo})]
-      (sut/register {:todo-repository todo-repository} todo)
-      (mockfn/verifying ()))))
+      (t/is (= (sut/register {:todo-repository todo-repository} todo) todo)))))
